286bec72be4798b6dcacc0f096f51dca8e0d745e
Nariman Suleimenov <162845160+NarSulEsz@users.noreply.github.com>
2024-08-01 20:33:47 -0400

    Added function to handle Routes requests

--- server.js
+++ server.js
@@ -8,8 +8,20 @@ const { v4: uuidv4 } = require('uuid');
 const app = express(); // Created an instance of Express object
 const PORT = process.env.PORT || 3001; // Created environment variable or  port 3001 as a default port
 
-// Middleware 
+
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
+// HTML Routes
+
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/notes.html')); // Send the notes.html file when the /notes route is accessed
+  });
+  
+ 
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html')); // Send the index.html file for all other routes (catch-all)
+  });
 
