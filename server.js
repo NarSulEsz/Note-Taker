a36ed65651fc9d1ad834749043a72f33ec463f0a
Nariman Suleimenov <162845160+NarSulEsz@users.noreply.github.com>
2024-08-01 20:13:02 -0400

    Added Middleware

--- server.js
+++ server.js
@@ -7,3 +7,9 @@ const { v4: uuidv4 } = require('uuid');
 
 const app = express(); // Created an instance of Express object
 const PORT = process.env.PORT || 3001; // Created environment variable or  port 3001 as a default port
+
+// Middleware 
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
