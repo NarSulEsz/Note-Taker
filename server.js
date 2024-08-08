77fdf585b547d853ef84c93022ac250ef4d366b6
Nariman Suleimenov <162845160+NarSulEsz@users.noreply.github.com>
2024-08-08 18:01:47 -0400

    fixed route order

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Importation of modules for further work
-const express = require('express'); 
-const fs = require('fs'); 
-const path = require('path'); 
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
 const { v4: uuidv4 } = require('uuid');
 
 
@@ -9,7 +9,7 @@ const app = express(); // Created an instance of Express object
 const PORT = process.env.PORT || 3001; // Created environment variable or  port 3001 as a default port
 
 
-app.use(express.json()); 
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
@@ -17,52 +17,49 @@ app.use(express.static('public'));
 
 //Added functions to handle Routes requests conserning html
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/notes.html')); //This if  /notes route is given
-  });
-  
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html')); //This if  any other routes  is given
-  });
-
+  res.sendFile(path.join(__dirname, 'public/notes.html')); //This if  /notes route is given
+});
 
 // GET route to obtain notes that already saved
 app.get('/api/notes', (req, res) => {
-    fs.readFile('db/db.json', 'utf8', (err, data) => {
+  fs.readFile('db/db.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('Error reading notes');
+    }
+    res.json(JSON.parse(data));
+  });
+});
+
+// POST route for saving any new note
+app.post('/api/notes', (req, res) => {
+  const newNote = { id: uuidv4(), ...req.body };
+
+  fs.readFile('db/db.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('Error reading notes');
+    }
+
+    const notes = JSON.parse(data);
+    notes.push(newNote);
+
+    fs.writeFile('db/db.json', JSON.stringify(notes, null, 2), (err) => {
       if (err) {
         console.error(err);
-        return res.status(500).send('Error reading notes');
+        return res.status(500).send('Error saving note');
       }
-      res.json(JSON.parse(data)); 
+      res.json(newNote);
     });
   });
-  
-  // POST route for saving any new note
-  app.post('/api/notes', (req, res) => {
-    const newNote = { id: uuidv4(), ...req.body };
-  
-    fs.readFile('db/db.json', 'utf8', (err, data) => { 
-      if (err) {
-        console.error(err);
-        return res.status(500).send('Error reading notes');
-      }
-  
-      const notes = JSON.parse(data); 
-      notes.push(newNote);
-  
-      fs.writeFile('db/db.json', JSON.stringify(notes, null, 2), (err) => {
-        if (err) {
-          console.error(err);
-          return res.status(500).send('Error saving note');
-        }
-        res.json(newNote);
-      });
-    });
-  });
+});
 
 
-
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html')); //This if  any other routes  is given
+});
 
 //Starting of server
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-  });
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
